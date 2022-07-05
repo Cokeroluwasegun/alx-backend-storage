e1ae5148d24951b0f0b49db77976ae4036d4c71c
Oluwasegun Coker <cokeroluwasegun@outlook.com>
2022-07-05 10:54:18 +0100

    Created all files

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,2 +1,8 @@
-// a script that deletes all documents with name="Holberton school" in the collection school
-db.school.deleteMany({name: "Holberton school"})
+#!/usr/bin/env python3
+""" List all documents in Python """
+
+
+def list_all(mongo_collection):
+    """ a function that lists all documents in a collection """
+    l = mongo_collection.find()
+    return l if l else []
